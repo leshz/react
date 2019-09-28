ac7f0df7cb9fdafa94bdc9bc5f04ebf4cce73447
Jeff Barragan <leshz@me.com>
2019-09-28 16:23:12 -0500

    Configuracion de ambiente

---
+++ src/components/test/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from "../../assets/app.scss";
+
+const Test = () => {
+    return (
+        <div>
+            <h2>TEST ONE TWO THREE !</h2>
+        </div>
+    )
+}
+
+export default Test
